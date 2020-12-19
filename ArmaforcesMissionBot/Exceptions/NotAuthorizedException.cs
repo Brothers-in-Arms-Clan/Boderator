b7c971441191ed75bd55bc164e80add1da121400
3Mydlo3 <mydlo333@interia.pl>
2020-12-19 12:36:41 +0100

    Fix NotAuthorizedException default message

--- ArmaforcesMissionBot/Exceptions/NotAuthorizedException.cs
+++ ArmaforcesMissionBot/Exceptions/NotAuthorizedException.cs
@@ -2,7 +2,7 @@
 
 namespace ArmaforcesMissionBot.Exceptions {
     public class NotAuthorizedException : Exception {
-        private const string DefaultMessage = "User is not authorized to use to perform this action!";
+        private const string DefaultMessage = "User is not authorized to perform this action!";
 
         public NotAuthorizedException() : this(DefaultMessage) {
         }
