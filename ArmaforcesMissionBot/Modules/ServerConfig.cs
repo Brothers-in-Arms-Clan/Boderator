aa25d15e9b0d7f376e60a6f8f11824dcf0f3416c
3Mydlo3 <mydlo333@interia.pl>
2021-01-20 18:44:25 +0100

    Add description for uploading json config

--- ArmaforcesMissionBot/Modules/ServerConfig.cs
+++ ArmaforcesMissionBot/Modules/ServerConfig.cs
@@ -16,7 +16,7 @@ namespace ArmaforcesMissionBot.Modules
         [ContextDMOrChannel]
         public override Task GetModsetConfig(string modsetName) => base.GetModsetConfig(modsetName);
 
-        [Summary("Wrzuca config serwera dla danego modsetu.")]
+        [Summary("Wrzuca config serwera dla danego modsetu. Config może być załączony w pliku *.json albo jako część wiadomości w formacie JSON po nazwie modsetu.")]
         [ContextDMOrChannel]
         public override Task PutModsetConfig(string modsetName, [Remainder] string configContent = null) => base.PutModsetConfig(modsetName, configContent);
 
@@ -24,7 +24,7 @@ namespace ArmaforcesMissionBot.Modules
         [ContextDMOrChannel]
         public override Task GetServerConfig() => base.GetServerConfig();
 
-        [Summary("Wrzuca główny config serwera.")]
+        [Summary("Wrzuca główny config serwera. Config może być załączony w pliku *.json albo jako część wiadomości w formacie JSON.")]
         [ContextDMOrChannel]
         public override Task PutServerConfig([Remainder] string configContent = null) => base.PutServerConfig(configContent);
     }
