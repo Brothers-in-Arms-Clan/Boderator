2d025dfecb25c0c49f655ac2aa619db67247b057
3Mydlo3 <mydlo333@interia.pl>
2021-01-20 18:28:06 +0100

    Fix bot failing to respond on Manager requests

---
+++ ArmaforcesMissionBot/Modules/ServerConfig.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using ArmaForces.ArmaServerManager.Discord.Features.ServerConfig;
+using ArmaforcesMissionBot.Attributes;
+using Discord.Commands;
+
+namespace ArmaforcesMissionBot.Modules
+{
+    [Name("ArmaServerManager - Server Config")]
+    public class ServerConfig : ServerConfigModule
+    {
+        public ServerConfig(IConfigurationManagerClient configurationManagerClient) : base(configurationManagerClient)
+        {
+        }
+
+        [Summary("Pobiera config serwera dla danego modsetu.")]
+        [ContextDMOrChannel]
+        public override Task GetModsetConfig(string modsetName) => base.GetModsetConfig(modsetName);
+
+        [Summary("Wrzuca config serwera dla danego modsetu.")]
+        [ContextDMOrChannel]
+        public override Task PutModsetConfig(string modsetName, string configContent = null) => base.PutModsetConfig(modsetName, configContent);
+
+        [Summary("Pobiera główny config serwera.")]
+        [ContextDMOrChannel]
+        public override Task GetServerConfig() => base.GetServerConfig();
+
+        [Summary("Wrzuca główny config serwera.")]
+        [ContextDMOrChannel]
+        public override Task PutServerConfig(string configContent = null) => base.PutServerConfig(configContent);
+    }
+}
