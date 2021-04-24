e24116556fbde3dc2bb10d42603533e36b28b331
3Mydlo3 <mydlo333@interia.pl>
2021-04-24 22:04:39 +0200

    Start design of SignupsCreateRequest

---
+++ ArmaforcesMissionBot/Features/Signups/DTOs/SignupsCreateRequestDto.cs
@@ -0,0 +1,15 @@
+﻿#nullable enable
+using System;
+using Newtonsoft.Json;
+
+namespace ArmaforcesMissionBot.Features.Signups.DTOs
+{
+    public class SignupsCreateRequestDto
+    {
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public DateTime? SignupsCloseTime { get; set; }
+
+        [JsonProperty(Required = Required.Always)]
+        public MissionDto Mission { get; set; } = new MissionDto();
+    }
+}
---
+++ ArmaforcesMissionBot/Features/Signups/DTOs/TeamDto.cs
@@ -0,0 +1,15 @@
+﻿#nullable enable
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace ArmaforcesMissionBot.Features.Signups.DTOs
+{
+    public class TeamDto
+    {
+        [JsonProperty(Required = Required.Always)]
+        public string Name { get; set; } = string.Empty;
+
+        [JsonProperty(Required = Required.Always)]
+        public List<SlotDto> Slots { get; set; } = new List<SlotDto>();
+    }
+}
