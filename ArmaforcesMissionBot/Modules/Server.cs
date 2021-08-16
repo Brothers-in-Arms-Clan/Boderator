768550d3d9df0a7ea18c16978f86df9799b7f3ae
3Mydlo3 <mydlo333@interia.pl>
2021-08-16 22:37:26 +0200

    Add aliases for startServer command (#55)

--- ArmaforcesMissionBot/Modules/Server.cs
+++ ArmaforcesMissionBot/Modules/Server.cs
@@ -20,9 +20,29 @@ namespace ArmaforcesMissionBot.Modules
         [Summary("Pozwala natychmiast uruchomić serwer z zadanym modsetem. Na przykład: AF!startServer default.")]
         [ContextDMOrChannel]
         public override Task StartServer(string modsetName) => base.StartServer(modsetName);
+        
+        [Command("startSerwer")]
+        [Summary("Alias dla `AF!startServer`.")]
+        [ContextDMOrChannel]
+        public Task StartSerwer(string modsetName) => StartServer(modsetName);
+        
+        [Command("serverStart")]
+        [Summary("Alias dla `AF!startServer`.")]
+        [ContextDMOrChannel]
+        public Task ServerStart(string modsetName) => StartServer(modsetName);
 
         [Summary("Pozwala uruchomić serwer z zadanym modsetem o zadanej godzinie w danym dniu. Na przykład: AF!startServer default 2020-07-17T19:00.")]
         [ContextDMOrChannel]
         public override Task StartServer(string modsetName, DateTime? dateTime) => base.StartServer(modsetName, dateTime);
+        
+        [Command("startSerwer")]
+        [Summary("Alias dla `AF!startServer`.")]
+        [ContextDMOrChannel]
+        public Task StartSerwer(string modsetName, DateTime? dateTime) => StartServer(modsetName, dateTime);
+        
+        [Command("serverStart")]
+        [Summary("Alias dla `AF!startServer`.")]
+        [ContextDMOrChannel]
+        public Task ServerStart(string modsetName, DateTime? dateTime) => StartServer(modsetName, dateTime);
     }
 }
