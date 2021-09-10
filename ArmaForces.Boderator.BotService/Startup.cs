fe6d251b92b24fb9c944acc1cf4fc01bc123cacf
3Mydlo3 <mydlo333@interia.pl>
2021-09-10 19:27:31 +0200

    Remove MediatR (#57)

--- ArmaForces.Boderator.BotService/Startup.cs
+++ ArmaForces.Boderator.BotService/Startup.cs
@@ -1,5 +1,4 @@
 using ArmaForces.Boderator.BotService.Discord;
-using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -21,7 +20,6 @@ namespace ArmaForces.Boderator.BotService
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMediatR(typeof(Startup));
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
