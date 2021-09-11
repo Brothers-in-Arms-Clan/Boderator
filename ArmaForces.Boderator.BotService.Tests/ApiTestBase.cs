057884e14e5fd9e45bb6a1dc9cace2c964d41c62
3Mydlo3 <mydlo333@interia.pl>
2021-09-11 03:12:02 +0200

    Start work on TestApiServer

---
+++ ArmaForces.Boderator.BotService.Tests/ApiTestBase.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using ArmaForces.Boderator.BotService.Tests.TestCollections;
+using CSharpFunctionalExtensions;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using Xunit;
+
+namespace ArmaForces.Boderator.BotService.Tests
+{
+    [Collection(Collections.ApiTest)]
+    public abstract class ApiTestBase
+    {
+        private readonly HttpClient _httpClient;
+
+        protected IServiceProvider Provider { get; }
+
+        protected ApiTestBase(TestApiServiceFixture testApi)
+        {
+            _httpClient = testApi.HttpClient;
+
+            Provider = new ServiceCollection()
+                .BuildServiceProvider();
+        }
+
+        protected async Task<Result<T>> HttpGetAsync<T>(string path)
+        {
+            var httpResponseMessage = await _httpClient.GetAsync(path);
+            if (httpResponseMessage.IsSuccessStatusCode)
+            {
+                var content = await httpResponseMessage.Content.ReadAsStringAsync();
+                return JsonConvert.DeserializeObject<T>(content);
+            }
+
+            var responseBody = await httpResponseMessage.Content.ReadAsStringAsync();
+            var error = string.IsNullOrWhiteSpace(responseBody)
+                ? httpResponseMessage.ReasonPhrase
+                : responseBody;
+
+            return Result.Failure<T>(error);
+        }
+
+        protected async Task<Result> HttpPostAsync<T>(string path, T body)
+        {
+            var httpResponseMessage = await _httpClient.PostAsync(path, new StringContent(JsonConvert.SerializeObject(body)));
+            if (httpResponseMessage.IsSuccessStatusCode)
+            {
+                return Result.Success();
+            }
+
+            var responseBody = await httpResponseMessage.Content.ReadAsStringAsync();
+            var error = string.IsNullOrWhiteSpace(responseBody)
+                ? httpResponseMessage.ReasonPhrase
+                : responseBody;
+
+            return Result.Failure<T>(error);
+        }
+    }
+}
---
+++ ArmaForces.Boderator.BotService.Tests/TestCollections/Collections.cs
@@ -0,0 +1,7 @@
+namespace ArmaForces.Boderator.BotService.Tests.TestCollections
+{
+    internal static class Collections
+    {
+        public const string ApiTest = "TestAPI";
+    }
+}
