92108ee3841884553c34d6617a8ec6c63e99fbac
3Mydlo3 <mydlo333@interia.pl>
2021-09-11 11:16:50 +0200

    Replace Debug controller with Health controller

---
+++ ArmaForces.Boderator.BotService/Features/Health/HealthController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ArmaForces.Boderator.BotService.Features.Health
+{
+    [Route("api/[controller]")]
+    public class HealthController : Controller
+    {
+        [HttpGet("ping")]
+        public IActionResult Ping() => Ok("pong");
+    }
+}
